9623a38abaf821fcc840bd1b475df0115cb84109
regurisahithi <jayanta.dey@musquaretech.com>
2023-06-07 14:07:30 +0530

    Add all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//Components
+// import Home from "./Pages/Home";
+import NavBar from "./components/NavBar";
+// import Works from "./Pages/Works";
+// import Contact from "./Pages/Contact";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      {/* <Home />
+      <Works />
+      <Contact /> */}
+    </>
   );
 }
-
-export default App;
