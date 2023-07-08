2f7198998fbd0de14c2ad766663374399117ffb1
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-07-08 21:59:29 +0530

    Done Call function

--- src/components/MakeCall/Call.js
+++ src/components/MakeCall/Call.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import headset from "../../Assets/images/headset-svgrepo-com.svg";
+import "./style.css";
 
 export default function Call() {
-  const phoneNumber = "7679624307";
+  const phoneNumber = "9091385148";
   return (
     <>
-      <Box>
-        {/* Your other content */}
-        <Typography>
-          Call us at <a href={`tel:${phoneNumber}`}>{phoneNumber}</a>
-        </Typography>
-        {/* More content */}
+      <Box className="headsetIconBox">
+        <a href={`tel:${phoneNumber}`}>
+          <img src={headset} alt="Phone Icon" />
+        </a>
       </Box>
     </>
   );
