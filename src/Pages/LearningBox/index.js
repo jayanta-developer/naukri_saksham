238f1ef3e8926c1d40c43076f37bf4f02e7726fd
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-07-10 13:25:49 +0530

    Media box Done

--- src/Pages/LearningBox/index.js
+++ src/Pages/LearningBox/index.js
@@ -3,33 +3,67 @@ import LearningBoxCoverPhoto from "../../Assets/images/student-online-cute-young
 import { Box, Typography } from "@mui/material";
 import Polygon from "../../Assets/images/Polygon 1.svg";
 import WhatsAppImage from "../../Assets/images/WhatsApp Image 2023-06-29 at 5.13 1.png";
+import LiveSessions from "../../Assets/images/Live Session.png";
+import PracticeInterviews from "../../Assets/images/Practice Interview.png";
+import DoubtSolving from "../../Assets/images/Doubt Solving.png";
+import PlacementPortal from "../../Assets/images/Placement Portal.png";
 
 import "./style.css";
 
 export default function LearningBox() {
-  const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const [selectedTab, setSelectedTab] = useState(0);
 
-  const handleMouseEnter = (index) => {
-    setHoveredIndex(index);
-  };
+  const mediaImages = [
+    WhatsAppImage,
+    LiveSessions,
+    PracticeInterviews,
+    DoubtSolving,
+    PlacementPortal,
+  ];
+  const mediaText = [
+    {
+      text: "Comprehensive Learning Platform for an Enhanced Learning Experience",
+      subText:
+        "Our learning portal provides a wide range of resources, materials, assessments, and practice problems, ensuring the best learning experience.",
+    },
+    {
+      text: "Amplify Your Knowledge with Expert-Led Live Classes",
+      subText:
+        "Engage in weekly live sessions led by our experts to unlock your learning potential. Benefit from direct interaction with our instructors for an unparalleled and enriching learning experience.",
+    },
+    {
+      text: "Unlimited 1-on-1 Mock Interviews with Mentors from Top Tech Companies.",
+      subText:
+        "Get set for success with endless support! Mentorship sessions, Resume/Project reviews, Mock HR interviews, and Job Readiness sessions all available to you.",
+    },
+    {
+      text: "Instant Query Resolution with Live Doubt Assistance.",
+      subText:
+        "Receive immediate answers to your queries through our live doubt assistance. Our mentors are available via chat and video, ensuring that you never get stuck and can effectively manage your time.",
+    },
+    {
+      text: "Streamline Job Applications with our Comprehensive Placement Portal.",
+      subText:
+        "Easily manage your job applications through our placement portal. Keep track of companies, positions, ongoing tests, technical and HR interviews, and secure your dream job in no time. Additionally, gain exclusive access to our partner companies for more opportunities.",
+    },
+  ];
 
-  const handleMouseLeave = () => {
-    setHoveredIndex(-1);
+  const handleTabClick = (e, index) => {
+    setSelectedTab(index);
   };
 
   const tab = (text, index) => {
     const className =
-      hoveredIndex === index ? "contentTabItemOnHover" : "contentTabItem";
-    const polygonClass = hoveredIndex === index ? "polygon" : "polygonBlock";
+      selectedTab === index ? "contentTabItemOnHover" : "contentTabItem";
+    const polygonClass = selectedTab === index ? "polygon" : "polygonBlock";
     const contentText =
-      hoveredIndex === index
+      selectedTab === index
         ? "contentTabText poppins"
         : "contentText poppinsBold";
     return (
       <Box
         className={className}
-        onMouseEnter={() => handleMouseEnter(index)}
-        onMouseLeave={handleMouseLeave}
+        onClick={(e) => handleTabClick(e, index)}
         sx={{
           boxShadow:
             "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px",
@@ -79,7 +113,7 @@ export default function LearningBox() {
         <Box className="mediaBox">
           <Box className="mediaCover">
             <Box className="media">
-              <img className="mediaImage" src={WhatsAppImage} />
+              <img className="mediaImage" src={mediaImages[selectedTab]} />
             </Box>
           </Box>
           <Box className="media_Text">
@@ -88,13 +122,10 @@ export default function LearningBox() {
               sx={{ fontWeight: "bolder" }}
               variant="h5"
             >
-              Unlimited 1-on-1 Mock Interviews with Mentors from Top Tech
-              Companies
+              {mediaText[selectedTab].text}
             </Typography>
             <Typography className="mediaParagraph poppins" mt={3}>
-              Get set for success with endless support! Mentorship sessions,
-              Resume/Project reviews, Mock HR interviews, and Job Readiness
-              sessions all available to you.
+              {mediaText[selectedTab].subText}
             </Typography>
           </Box>
         </Box>
